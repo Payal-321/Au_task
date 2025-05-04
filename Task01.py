f62cdfa9ebb42dc30eace0fbf9f6f011cd2ddd3b
payal jangid <JANGIDPAYAL355@GMAIL.COM>
2025-05-04 15:19:55 +0530

    Add files via upload

--- Task01.py
+++ Task01.py
@@ -1,4 +1,5 @@
 ####1
+
 def celsius_to_fahrenheit(celsius):
     return (celsius * 9/5) + 32
 
@@ -6,3 +7,14 @@ celsius = float(input("Enter temperature in Celsius: "))
 fahrenheit = celsius_to_fahrenheit(celsius)
 
 print(f"{celsius}°C is equal to {fahrenheit}°F")
+
+###2
+def swap_to_numbers(num1,num2):
+    temp=num1
+    num1=num2
+    num2=temp
+    return swap_to_numbers
+
+num1=int(input("Enter a number:"))
+num2=int(input("Enter a number:"))
+print("After swapping:",num2,num1)
