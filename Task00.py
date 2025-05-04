857663da6d6385ea4d0c59de20aaae144d2ca995
payal jangid <JANGIDPAYAL355@GMAIL.COM>
2025-05-04 14:47:45 +0530

    Add files via upload

---
+++ Task00.py
@@ -0,0 +1,22 @@
+def add(a,b=0,c=6):
+    return a+b+c
+print(add(5,9))
+
+def fact(n):
+    total=1
+    for i in range(n):
+        total=total*i
+        return total
+
+    
+def fib(n):
+    if n==0 or n==1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+     
+def str(n):
+    for i in range(1,n):
+        print(i*"*")
+        
