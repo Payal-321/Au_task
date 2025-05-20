aab7440ed76bbe47feed2e8612a90b5a0c907ad0
payal jangid <JANGIDPAYAL355@GMAIL.COM>
2025-05-20 23:32:33 +0530

    Add files via upload

---
+++ DSAtask.py
@@ -0,0 +1,64 @@
+#Q1
+L=[1,2,3,4,5]
+sum=0
+for i in L:
+    sum=sum+1
+print(sum)
+product=1
+for i in L:
+    product=product*i
+print(product)
+
+#Q2
+for i in L:
+  for j in L:
+    print("{},{}".format(i,j))
+
+#Q3
+def intostr(i):
+  digit="0123456789"
+  if i==0:
+    return "0"
+  result=" "
+  while i>0:
+    result=digit[i%10]+result
+    i=i//10
+  return result
+intostr(234)
+
+#Q4
+L=[1,2,3,4,5]
+for i in range(len(L)):
+  for j in range(i+1,len(L)):
+    print("{},{}".format(i,j))
+
+#Q5
+A=[1,2,3,4]
+B=[2,3,4,5,6]
+for i in A:
+  for j in B:
+    if i<j:
+      print("{}.{}".format(i,j))
+
+#Q6
+A=[1,2,3,4]
+B=[2,3,4,5]
+for i in A:
+  for j in B:
+    for k in range(1):
+      print("{},{}".format(i,j))
+
+#Q7
+def factorial(n):
+  if n==1:
+    return 1
+  else:
+    return n*factorial(n-1)
+
+#Q8
+def fib(n):
+  if n==1 or n==0:
+    return 1
+  else:
+    return fib(n-1)+fib(n-2)
+
